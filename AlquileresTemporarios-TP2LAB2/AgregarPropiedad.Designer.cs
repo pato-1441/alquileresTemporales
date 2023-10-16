9b285d0483e0b28426de6a9425fb9fc640e827c6
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-10-16 18:08:02 -0300

    acomodando forms

--- AlquileresTemporarios-TP2LAB2/AgregarPropiedad.Designer.cs
+++ AlquileresTemporarios-TP2LAB2/AgregarPropiedad.Designer.cs
@@ -59,67 +59,75 @@
             "Hotel",
             "Casa",
             "Casa fin de semana"});
-            this.cbTipoPropiedad.Location = new System.Drawing.Point(24, 54);
+            this.cbTipoPropiedad.Location = new System.Drawing.Point(37, 56);
+            this.cbTipoPropiedad.Margin = new System.Windows.Forms.Padding(2);
             this.cbTipoPropiedad.Name = "cbTipoPropiedad";
-            this.cbTipoPropiedad.Size = new System.Drawing.Size(213, 24);
+            this.cbTipoPropiedad.Size = new System.Drawing.Size(161, 21);
             this.cbTipoPropiedad.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 23);
+            this.label1.Location = new System.Drawing.Point(34, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(120, 16);
+            this.label1.Size = new System.Drawing.Size(93, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Tipo de propiedad";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(26, 125);
+            this.label2.Location = new System.Drawing.Point(34, 89);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.Size = new System.Drawing.Size(51, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Provincia";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(26, 207);
+            this.label3.Location = new System.Drawing.Point(20, 168);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.Size = new System.Drawing.Size(0, 13);
             this.label3.TabIndex = 4;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(32, 194);
+            this.label4.Location = new System.Drawing.Point(35, 142);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(67, 16);
+            this.label4.Size = new System.Drawing.Size(53, 13);
             this.label4.TabIndex = 5;
             this.label4.Text = "Localidad";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(29, 290);
+            this.textBox1.Location = new System.Drawing.Point(37, 215);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(139, 22);
+            this.textBox1.Size = new System.Drawing.Size(105, 20);
             this.textBox1.TabIndex = 7;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(32, 261);
+            this.label5.Location = new System.Drawing.Point(35, 198);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(64, 16);
+            this.label5.Size = new System.Drawing.Size(52, 13);
             this.label5.TabIndex = 6;
             this.label5.Text = "Direccion";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(29, 224);
+            this.textBox2.Location = new System.Drawing.Point(37, 161);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(139, 22);
+            this.textBox2.Size = new System.Drawing.Size(105, 20);
             this.textBox2.TabIndex = 8;
             // 
             // cbProvincias
@@ -150,41 +158,46 @@
             "Santiago del Estero",
             "Tierra Del Fuego",
             "Tucumán"});
-            this.cbProvincias.Location = new System.Drawing.Point(29, 154);
+            this.cbProvincias.Location = new System.Drawing.Point(37, 104);
+            this.cbProvincias.Margin = new System.Windows.Forms.Padding(2);
             this.cbProvincias.Name = "cbProvincias";
-            this.cbProvincias.Size = new System.Drawing.Size(213, 24);
+            this.cbProvincias.Size = new System.Drawing.Size(161, 21);
             this.cbProvincias.TabIndex = 9;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(343, 39);
+            this.label6.Location = new System.Drawing.Point(228, 38);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(141, 16);
+            this.label6.Size = new System.Drawing.Size(110, 13);
             this.label6.TabIndex = 10;
-            this.label6.Text = "Cantidad de Personas";
+            this.label6.Text = "Cantidad de personas";
             // 
             // nudCantPersonas
             // 
-            this.nudCantPersonas.Location = new System.Drawing.Point(346, 59);
+            this.nudCantPersonas.Location = new System.Drawing.Point(231, 57);
+            this.nudCantPersonas.Margin = new System.Windows.Forms.Padding(2);
             this.nudCantPersonas.Name = "nudCantPersonas";
-            this.nudCantPersonas.Size = new System.Drawing.Size(120, 22);
+            this.nudCantPersonas.Size = new System.Drawing.Size(90, 20);
             this.nudCantPersonas.TabIndex = 11;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(343, 125);
+            this.label7.Location = new System.Drawing.Point(228, 89);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(109, 16);
+            this.label7.Size = new System.Drawing.Size(88, 13);
             this.label7.TabIndex = 12;
             this.label7.Text = "Precio por noche";
             // 
             // tbPrecioXNoche
             // 
-            this.tbPrecioXNoche.Location = new System.Drawing.Point(346, 145);
+            this.tbPrecioXNoche.Location = new System.Drawing.Point(231, 105);
+            this.tbPrecioXNoche.Margin = new System.Windows.Forms.Padding(2);
             this.tbPrecioXNoche.Name = "tbPrecioXNoche";
-            this.tbPrecioXNoche.Size = new System.Drawing.Size(139, 22);
+            this.tbPrecioXNoche.Size = new System.Drawing.Size(105, 20);
             this.tbPrecioXNoche.TabIndex = 13;
             // 
             // groupBox1
@@ -195,11 +208,11 @@
             this.groupBox1.Controls.Add(this.checkBox3);
             this.groupBox1.Controls.Add(this.checkBox2);
             this.groupBox1.Controls.Add(this.checkBox1);
-            this.groupBox1.Location = new System.Drawing.Point(363, 201);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Location = new System.Drawing.Point(231, 145);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.groupBox1.Size = new System.Drawing.Size(272, 111);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(241, 90);
             this.groupBox1.TabIndex = 16;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Características";
@@ -207,10 +220,10 @@
             // checkBox6
             // 
             this.checkBox6.AutoSize = true;
-            this.checkBox6.Location = new System.Drawing.Point(168, 33);
-            this.checkBox6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.checkBox6.Location = new System.Drawing.Point(148, 27);
+            this.checkBox6.Margin = new System.Windows.Forms.Padding(2);
             this.checkBox6.Name = "checkBox6";
-            this.checkBox6.Size = new System.Drawing.Size(91, 20);
+            this.checkBox6.Size = new System.Drawing.Size(74, 17);
             this.checkBox6.TabIndex = 18;
             this.checkBox6.Text = "Desayuno";
             this.checkBox6.UseVisualStyleBackColor = true;
@@ -218,10 +231,10 @@
             // checkBox5
             // 
             this.checkBox5.AutoSize = true;
-            this.checkBox5.Location = new System.Drawing.Point(11, 69);
-            this.checkBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.checkBox5.Location = new System.Drawing.Point(19, 56);
+            this.checkBox5.Margin = new System.Windows.Forms.Padding(2);
             this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(63, 20);
+            this.checkBox5.Size = new System.Drawing.Size(52, 17);
             this.checkBox5.TabIndex = 17;
             this.checkBox5.Text = "Pileta";
             this.checkBox5.UseVisualStyleBackColor = true;
@@ -229,10 +242,10 @@
             // checkBox4
             // 
             this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(168, 69);
-            this.checkBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.checkBox4.Location = new System.Drawing.Point(148, 56);
+            this.checkBox4.Margin = new System.Windows.Forms.Padding(2);
             this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(101, 20);
+            this.checkBox4.Size = new System.Drawing.Size(81, 17);
             this.checkBox4.TabIndex = 16;
             this.checkBox4.Text = "Pet-Friendly";
             this.checkBox4.UseVisualStyleBackColor = true;
@@ -240,10 +253,10 @@
             // checkBox3
             // 
             this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(79, 69);
-            this.checkBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.checkBox3.Location = new System.Drawing.Point(75, 56);
+            this.checkBox3.Margin = new System.Windows.Forms.Padding(2);
             this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(83, 20);
+            this.checkBox3.Size = new System.Drawing.Size(67, 17);
             this.checkBox3.TabIndex = 15;
             this.checkBox3.Text = "Limpieza";
             this.checkBox3.UseVisualStyleBackColor = true;
@@ -251,10 +264,10 @@
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(79, 33);
-            this.checkBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.checkBox2.Location = new System.Drawing.Point(75, 27);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(2);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(80, 20);
+            this.checkBox2.Size = new System.Drawing.Size(66, 17);
             this.checkBox2.TabIndex = 14;
             this.checkBox2.Text = "Cochera";
             this.checkBox2.UseVisualStyleBackColor = true;
@@ -262,19 +275,19 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(11, 33);
-            this.checkBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.checkBox1.Location = new System.Drawing.Point(19, 27);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(56, 20);
+            this.checkBox1.Size = new System.Drawing.Size(50, 17);
             this.checkBox1.TabIndex = 13;
-            this.checkBox1.Text = "WiFi";
+            this.checkBox1.Text = "Wi-Fi";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // AgregarPropiedad
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(527, 339);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.tbPrecioXNoche);
             this.Controls.Add(this.label7);
@@ -289,6 +302,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cbTipoPropiedad);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "AgregarPropiedad";
             this.Text = "AgregarPropiedad";
             ((System.ComponentModel.ISupportInitialize)(this.nudCantPersonas)).EndInit();

75bdf79c1614c3d6241c859055ec1f5f42c84d06
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-10-16 18:33:50 -0300

    alineamiento cliente

--- AlquileresTemporarios-TP2LAB2/RegistrarCliente.Designer.cs
+++ AlquileresTemporarios-TP2LAB2/RegistrarCliente.Designer.cs
@@ -38,18 +38,20 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(22, 145);
+            this.button1.Location = new System.Drawing.Point(17, 129);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(85, 42);
+            this.button1.Size = new System.Drawing.Size(76, 34);
             this.button1.TabIndex = 0;
             this.button1.Text = "Agregar";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(143, 145);
+            this.button2.Location = new System.Drawing.Point(97, 129);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(85, 42);
+            this.button2.Size = new System.Drawing.Size(76, 34);
             this.button2.TabIndex = 1;
             this.button2.Text = "Cancelar";
             this.button2.UseVisualStyleBackColor = true;
@@ -57,46 +59,51 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 33);
+            this.label1.Location = new System.Drawing.Point(14, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.Size = new System.Drawing.Size(44, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Nombre";
             // 
             // tbNombre
             // 
-            this.tbNombre.Location = new System.Drawing.Point(128, 33);
+            this.tbNombre.Location = new System.Drawing.Point(17, 40);
+            this.tbNombre.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbNombre.Name = "tbNombre";
-            this.tbNombre.Size = new System.Drawing.Size(100, 22);
+            this.tbNombre.Size = new System.Drawing.Size(156, 20);
             this.tbNombre.TabIndex = 3;
             // 
             // tbDNI
             // 
-            this.tbDNI.Location = new System.Drawing.Point(128, 80);
+            this.tbDNI.Location = new System.Drawing.Point(17, 93);
+            this.tbDNI.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbDNI.Name = "tbDNI";
-            this.tbDNI.Size = new System.Drawing.Size(100, 22);
+            this.tbDNI.Size = new System.Drawing.Size(156, 20);
             this.tbDNI.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(19, 86);
+            this.label2.Location = new System.Drawing.Point(14, 74);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(30, 16);
+            this.label2.Size = new System.Drawing.Size(26, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "DNI";
             // 
             // RegistrarCliente
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(271, 241);
+            this.ClientSize = new System.Drawing.Size(195, 177);
             this.Controls.Add(this.tbDNI);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.tbNombre);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "RegistrarCliente";
             this.Text = "RegistrarCliente";
             this.ResumeLayout(false);
